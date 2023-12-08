a5440309fad37856454010b1884f4bf3ffa1c113
Ivan Borovyk <Ivan_Borovyk@epam.com>
2023-12-08 08:32:16 +0200

    refactored

--- src/4.ts
+++ src/4.ts
@@ -1,56 +1,78 @@
-interface IKey {
-    isKey: boolean;
-}
-
-interface IHouse {
-    openDoor(key: IKey): void;
-    comeIn(person: IPerson): void;
-}
-
-interface IPerson {
-    getKey(): IKey;
-}
-
 class Key {
-    private _isKey: boolean = true;
+    private signature: number;
 
-    public get isKey(): boolean {
-        return this._isKey;
+    constructor() {
+        this.signature = Math.random();
+    }
+
+    public getSignature(): number {
+        return this.signature;
     }
 }
 
-class MyHouse implements IHouse {
-    constructor(private key: IKey) {}
 
-    public openDoor(key: IKey): void {
-        if (key.isKey) {
-            console.log('Open door');
-        } else {
-            console.log('You can not open door');
-        }
-    }
+class Person {
+    constructor(private key: Key) {}
 
-    public comeIn(person: IPerson): void {
-        this.openDoor(person.getKey());
-    }
-}
-
-class Person implements IPerson {
-    constructor(private key: IKey) {}
-
-    public getKey(): IKey {
+    public getKey(): Key {
         return this.key;
     }
 }
 
-const key = new Key();
+abstract class House {
+    private tenants: Person[] = [];
+    protected door: boolean = false;
 
-const house = new MyHouse(key);
-const person = new Person(key);
+    constructor(protected key: Key) {}
 
-house.openDoor(person.getKey());
+    public comeIn(person: Person): void {
+        if (this.door) {
+            this.tenants.push(person);
+        }
+    }
 
-house.comeIn(person);
+    abstract openDoor(key: Key): void
+}
+
+class MyHouse extends House {
+    constructor(key: Key) {
+        super(key);
+    }
+    openDoor(key: Key): void {
+        if (key.getSignature() === this.key.getSignature()) {
+            this.door = true;
+        }
+    }
+}
+const key1 = new Key();
+
+const house1 = new MyHouse(key1);
+const person1 = new Person(key1);
+
+house1.openDoor(person1.getKey());
+
+house1.comeIn(person1);
+
+const key2  = new Key();
+const person2 = new Person(key2);
+const house2 = new MyHouse(key2);
+
+house2.comeIn(person2);
+house2.comeIn(person1);
+
+console.log(house2); // tenants still empty
+
+
+house2.openDoor(person1.getKey()); // shouldn't be open
+house2.openDoor(person2.getKey()); // now the door is open
+
+house2.comeIn(person2);
+house2.comeIn(person1);
+
+console.log(house2); // tenants.length === 2
+
+
+
 
 
 export {};
