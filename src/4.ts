40053b0c30c4b1c9dc768daf2864b002e6dfd41a
Ivan Borovyk <Ivan_Borovyk@epam.com>
2023-12-08 08:11:38 +0200

    added solution

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,48 @@
+interface IKey {
+    isKey: boolean;
+}
+
+interface IHouse {
+    openDoor(key: IKey): void;
+    comeIn(person: IPerson): void;
+}
+
+interface IPerson {
+    getKey(): IKey;
+}
+
+class Key {
+    private _isKey: boolean = true;
+
+    public get isKey(): boolean {
+        return this._isKey;
+    }
+}
+
+class MyHouse implements IHouse {
+    constructor(private key: IKey) {}
+
+    public openDoor(key: IKey): void {
+        if (key.isKey) {
+            console.log('Open door');
+        } else {
+            console.log('You can not open door');
+        }
+    }
+
+    public comeIn(person: IPerson): void {
+        this.openDoor(person.getKey());
+    }
+}
+
+class Person implements IPerson {
+    constructor(private key: IKey) {}
+
+    public getKey(): IKey {
+        return this.key;
+    }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
